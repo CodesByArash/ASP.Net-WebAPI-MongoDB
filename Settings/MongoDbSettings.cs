1484f9c06ebb048154e9a7d231b91e509a653b37
arash <arash.m.gholinejad@gmail.com>
2025-08-24 19:25:40 +0330

    Added mongodbsettings

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace ASP.MongoDb.API.Settings
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
